6a888b6919b39a75a79b7b89690fcff1918a2353
Daniel Garbień <dga@trifork.com>
2015-08-13 15:36:02 +0200

    Save Core Data context when application resigns active.

--- LetHerKnow/AppDelegate.swift
+++ LetHerKnow/AppDelegate.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 import Contacts
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var coreDataStack: CoreDataStack!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        coreDataStack = CoreDataStack(momResourceName: "Model", storeFileName: "Model")
         
         // Workaround for a beta 4 bug, permission should not be required, removed after it is resolved
         // https://forums.developer.apple.com/thread/12275
@@ -23,5 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         cn.requestAccessForEntityType(CNEntityType.Contacts) { (success: Bool, error: NSError?) -> Void in }
         
         return true
+    }
+    
+    func applicationWillResignActive(application: UIApplication) {
+        coreDataStack.save()
     }
 }
