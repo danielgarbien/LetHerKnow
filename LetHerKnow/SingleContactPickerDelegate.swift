7a53d965db5ef96ab1268934d4e8ad7290681065
Daniel Garbień <dga@trifork.com>
2015-08-14 14:01:51 +0200

    Add Single contact picker delegate.

---
+++ LetHerKnow/SingleContactPickerDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SingleContactPickerDelegate.swift
+//  LetHerKnow
+//
+//  Created by Daniel Garbień on 14/08/15.
+//  Copyright © 2015 Daniel Garbień. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import ContactsUI
+
+class SingleContactPickerDelegate: NSObject, CNContactPickerDelegate {
+    
+    private let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    func contactPicker(picker: CNContactPickerViewController, didSelectContactProperty contactProperty: CNContactProperty) {
+        let contact = contactProperty.contact
+        if let phoneNumber = contactProperty.value as? CNPhoneNumber {
+            context.setSoulmateWithIdentifier(contact.identifier,
+                displayName: contact.displayName(),
+                phoneNumber: phoneNumber.stringValue)
+        } else {
+            assertionFailure()
+        }
+    }
+    
+    func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
+        if let phoneNumber = contact.firstPhoneNumber() {
+            context.setSoulmateWithIdentifier(contact.identifier,
+                displayName: contact.displayName(),
+                phoneNumber: phoneNumber)
+        } else {
+            assertionFailure()
+        }
+    }
+}
